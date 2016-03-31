f9f2fbf0689ecaaa91497f085402443a7904f278
Ivo Verberk <iverberk@xebia.com>
2016-03-31 20:58:45 +0200

    Create static binaries

--- services/age/build.sh
+++ services/age/build.sh
@@ -5,8 +5,10 @@ set -e
 cd ${0%/*}
 
 go get -d -v
+
 go test
-go build -o age-service main.go
+
+CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o age-service main.go
 
 docker build -t registry.service.consul:5000/iverberk/age-service .
 
--- services/name/build.sh
+++ services/name/build.sh
@@ -5,8 +5,10 @@ set -e
 cd ${0%/*}
 
 go get
+
 go test
-go build -o name-service main.go
+
+CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o name-service main.go
 
 docker build -t registry.service.consul:5000/iverberk/name-service .
 
