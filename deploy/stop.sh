94af03a112ffe690f65d0440d81ffa76cc995b1a
Ivo Verberk <iverberk@xebia.com>
2016-04-03 14:16:04 +0200

    Silent output

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -10,5 +10,5 @@ fi
 jobs=("micro-app" "name-service" "age-service" "redis")
 for job in "${jobs[@]}"
 do
-    curl -XDELETE --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/$job-$ENV
+    curl --silent -XDELETE --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/$job-$ENV
 done
--- integration-tests/run_tests.sh
+++ integration-tests/run_tests.sh
@@ -2,8 +2,8 @@
 
 # Start Firefox and Chrome browser nodes
 echo "Starting Selenium browser nodes"
-curl -XPUT -d @jobs/selenium-chrome.json --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-chrome
-curl -XPUT -d @jobs/selenium-firefox.json --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-firefox
+curl --silent -XPUT -d @jobs/selenium-chrome.json --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-chrome
+curl --silent -XPUT -d @jobs/selenium-firefox.json --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-firefox
 
 # Spin up integration environment
 echo "Starting integration environment"
@@ -17,7 +17,7 @@ echo "Waiting for platform to come online"
 sleep 20
 
 # Set the base url for our micro-app from Consul
-BASE_URL=$(curl -XGET 192.168.10.10:8500/v1/catalog/service/micro-app-integration | jq -r '.[0] | .Address + ":" + (.ServicePort | tostring) ')
+BASE_URL=$(curl --silent -XGET 192.168.10.10:8500/v1/catalog/service/micro-app-integration | jq -r '.[0] | .Address + ":" + (.ServicePort | tostring) ')
 sed -i "s/###BASE_URL###/$BASE_URL/g" wdio.conf.js
 
 # Run tests
@@ -26,8 +26,8 @@ wdio wdio.conf.js
 
 # Tear down selenium browser nodes
 echo "Stopping integration environment and browser nodes"
-curl -XDELETE --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-firefox
-curl -XDELETE --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-chrome
+curl --silent -XDELETE --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-firefox
+curl --silent -XDELETE --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-chrome
 
 # Stop integration environent
 ENV=integration ../deploy/stop.sh
