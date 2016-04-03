f43e261713242b6c51dfe170378cf9350080ee58
Ivo Verberk <iverberk@xebia.com>
2016-04-03 12:46:04 +0200

    timing

--- integration-tests/run_tests.sh
+++ integration-tests/run_tests.sh
@@ -4,14 +4,14 @@
 curl -XPUT -d @jobs/selenium-chrome.json --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-chrome
 curl -XPUT -d @jobs/selenium-firefox.json --header "Content-Type: application/json" 192.168.10.10:4646/v1/job/selenium-firefox
 
-# Wait for nodes to come online (should be improved)
-sleep 10
-
 # Spin up integration environment
 ENV=integration ../deploy/deploy.sh
 
 # Install some additional npm libraries
 npm install chai
+
+# Wait for platform and nodes to come online (should be improved)
+sleep 10
 
 # Run tests
 wdio wdio.conf.js
