7985e999fd715f5111981346b7d80b8e6fce3c33
Ivo Verberk <iverberk@xebia.com>
2016-04-03 14:02:27 +0200

    dsg

--- integration-tests/specs/simple.js
+++ integration-tests/specs/simple.js
@@ -1,9 +1,6 @@
 describe('the micro-app application', function() {
     it('should generate an introduction', function() {
-        browser
-            .url('/')
-            .getSource().then(function(source) {
-                source.should.match(/Hello, my name is .+ and I'm \d+ years old and I live in the .+ environment!/)
-            });
+        browser.url('/');
+        browser.getTitle().should.match(/Hello, my name is .+ and I'm \d+ years old and I live in the .+ environment!/);
     });
 });
