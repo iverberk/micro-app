3cec698a0d9a7b109b9b13ab05d62b79b9987a99
Ivo Verberk <iverberk@xebia.com>
2016-04-04 11:28:15 +0200

    ds

--- integration-tests/specs/simple.js
+++ integration-tests/specs/simple.js
@@ -1,12 +1,7 @@
 describe('the micro-app application', function() {
     it('should generate an introduction', function() {
-        client
-            .url('http://webdriver.io')
-            .getSource().then(function(source) {
-                console.log(source); // outputs: "<!DOCTYPE html>\n<title>Webdriver.io</title>..."
-            });
-        //browser.url('/');
-        //browser.getText('h1*=Hello').then(function(text) {
-           //text.should.match(/Hello, my name is .+ and I'm \d+ years old and I live in the .+ environment!/);
-        //});
+        browser.url('/');
+        browser.getText('#intro').then(function(text) {
+            text.should.match(/Hello, my name is .+ and I'm \d+ years old and I live in the .+ environment!/);
+        });
 });
